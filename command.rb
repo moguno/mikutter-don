16788310b34703170d01b80f88db11334f014060
moguno <moguno@0kn.sakura.ne.jp>
2017-05-23 23:39:42 +0900

    投稿コマンドを追加

--- command.rb
+++ command.rb
@@ -1,4 +1,17 @@
 Plugin.create(:"mikutter丼") {
+  # 極限まで手を抜いた投稿コマンド
+  command(:don_post,
+          :name => _("Mastodonに投稿"),
+          :condition => lambda { |opt| true },
+          :visible => true,
+          :role => :postbox) { |opt|
+     postbox = Plugin[:gtk].widgetof(opt.widget)
+     text = postbox.widget_post.buffer.text
+
+     @client.create_status(text)
+     postbox.widget_post.buffer.text = ""
+  }
+ 
   # ふぁぼ
   command(:don_fav,
           :name => _("ふぁぼる"),
